d23c47b28ba0b79f6d0941e54e026802e6c81b0f
Mike Miller <mike.d.miller1@gmail.com>
2016-12-27 14:04:08 -0500

    - Initial commit

--- InternetMonitor/Program.cs
+++ InternetMonitor/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InternetMonitor
@@ -16,7 +13,7 @@ namespace InternetMonitor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault( false );
-            Application.Run( new Form1() );
+            Application.Run( new MainWindow() );
         }
     }
 }
---
+++ InternetMonitor/Results.cs
@@ -0,0 +1,6 @@
+﻿namespace InternetMonitor
+{
+    class Results
+    {
+    }
+}
